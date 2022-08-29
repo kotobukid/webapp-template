fc04cb8c309572af7be9680ff22e5f06680880a3
kotobukid <kotobuki@daqua.jp>
2022-08-29 18:55:52 +0900

    Vite(vue-ts)の最小構成プラスアルファを移植

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template lang="pug">
+HelloWorld(msg="Vite + Vue + Express + Socket.io" v-cloak)
+</template>
+
+<style scoped lang="less">
+.button {
+    user-select: none;
+    -webkit-user-drag: none;
+    border-radius: 4px;
+    border: 1px solid grey;
+    padding: 4px;
+
+    &:hover {
+        background-color: lightgreen;
+        border-color: green;
+        color: green;
+    }
+
+    &:active {
+        position: relative;
+        top: 1px;
+        color: red;
+    }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>()
+</script>
+
+<template lang="pug">
+h1 {{ msg }}
+</template>
+
+<style scoped>
+
+</style>
